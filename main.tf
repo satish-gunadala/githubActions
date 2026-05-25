cac64a314f91a37bd25a0e6089ce8b598d09833c
satish <gunadalasatish@gmail.com>
2026-05-25 23:46:20 +0530

    practicing gitops

---
+++ main.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "git_hub_self_runner" {
+    ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI (HVM), SSD Volume Type
+    instance_type = "t2.micro"
+    key_name      = "your-key-pair-name" # Replace with your key pair name
+    
+    tags = {
+        Name = "GitHub Self-Hosted Runner"
+        Name = "GitHub Self-Hosted Runner2"
+    }
+}
