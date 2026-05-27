b1d5e15b15ec641ffd7afb8ba97dbc32be571cb5
satish <gunadalasatish@gmail.com>
2026-05-27 22:56:51 +0530

    test commit to check if jekins triggerss

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
 resource "aws_instance" "git_hub_self_runner" {
     ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI (HVM), SSD Volume Type
     instance_type = "t2.micro"
