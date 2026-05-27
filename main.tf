467837abfe8265f60fb33ebb6efbf8633c41db30
satish <gunadalasatish@gmail.com>
2026-05-27 23:08:47 +0530

    adding null resource

--- main.tf
+++ main.tf
@@ -1,17 +1,11 @@
-provider "aws" {
-  region = "ap-south-1"
+terraform {
+
+  required_providers {
+    null = {
+      source = "hashicorp/null"
+      version = "~> 3.0"
+    }
+  }
 }
 
-resource "aws_instance" "git_hub_self_runner" {
-    ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI (HVM), SSD Volume Type
-    instance_type = "t2.micro"
-    key_name      = "your-key-pair-name" # Replace with your key pair name
-    
-    tags = {
-        Name = "GitHub Self-Hosted Runner"
-        Name = "GitHub Self-Hosted Runner2"
-        Name = "GitHub Self-Hosted Runner3"
-        Name = "GitHub Self-Hosted Runner4"
-        Name = "GitHub Self-Hosted Runner5"
-    }
-}
+resource "null_resource" "demo" {}
